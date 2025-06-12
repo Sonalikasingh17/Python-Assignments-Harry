9afe66e3aa753ef1c73ba532388ac4d0f85611ab
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-06-12 08:58:07 +0530

    Update 02_problem2.py

--- Chapter 4/02_problem2.py
+++ Chapter 4/02_problem2.py
@@ -12,7 +12,7 @@ s5 = int(input("Enter student marks: "))
 students_marks.append(s5)
 s6 = int(input("Enter student marks: "))
 students_marks.append(s6)
-
+ 
 students_marks.sort()
 
 print(students_marks)
