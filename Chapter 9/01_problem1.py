7efb4b1148dd25ee2fb649f5d43532b9c53e5c25
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-07-18 20:55:41 +0530

    Update 01_problem1.py

--- Chapter 9/01_problem1.py
+++ Chapter 9/01_problem1.py
@@ -3,7 +3,7 @@
 
 f = open("poem.txt","r")
 content = f.read()
-if("Twinkle" in content):
+if("Twinkle" in content): 
     print("The word twinkle is present in the content")
 else:
     print("The word twinkle is not present in the content")
