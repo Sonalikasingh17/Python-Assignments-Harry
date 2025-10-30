c0e11bd15319d694cf224bd4a9fb7c7b12524303
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-10-30 23:40:51 +0530

    Fix formatting and indentation in star pattern program

--- Chapter 7/09_problem9.py
+++ Chapter 7/09_problem9.py
@@ -2,7 +2,7 @@
 * * *
 *  * for n = 3
 * * * '''
-
+ 
 ''' for n = 5
 *****
 *   *
@@ -18,4 +18,5 @@ for i in range(1, n+1):
         print("*", end = "")
         print(" "*(n-2), end ="")
         print("*", end = "")
+
     print("")
